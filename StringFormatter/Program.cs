cca787a33eb4893f140b97cd9b38e6ed943b81a0
MINJUN17 <71377165+MINJUN17@users.noreply.github.com>
2026-03-10 15:29:20 +0900

    코드수정

--- StringFormatter/Program.cs
+++ StringFormatter/Program.cs
@@ -4,15 +4,14 @@ Func<string, string> toUpper = text => text.ToUpper();
 Func<string, string> mask = text => $"{text[0]}{text[1]}***";
 Func<string, string> Greet = text => $"안녕하세요, {text}님!";
 
-Console.Write($"[대문자 변환] hello world -> ");
-ApplyAndPrint("hello world", toUpper);
+Console.WriteLine($"[대문자 변환] hello world -> {ApplyAndPrint("hello world", toUpper)}");
 
-Console.Write($"[마스킹 처리] SecretCode -> ");
-ApplyAndPrint("SecretCode", mask);
 
-Console.Write($"[인사말 생성] 민수 -> ");
-ApplyAndPrint("민수", Greet);
-void ApplyAndPrint(string input, Func<string, string> formatter)
+Console.WriteLine($"[마스킹 처리] SecretCode -> {ApplyAndPrint("SecretCode", mask)}");
+
+
+Console.WriteLine($"[인사말 생성] 민수 -> {ApplyAndPrint("민수", Greet)}");
+string ApplyAndPrint(string input, Func<string, string> formatter)
 {
-    Console.WriteLine(formatter(input));
+    return formatter(input);
 }
